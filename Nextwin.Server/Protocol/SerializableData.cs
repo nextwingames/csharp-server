c19d6b3d1f9cbe2aab1c5957ed9cc470ce488a00
hyeonQyu <hyeonQyu@github.com>
2021-01-02 22:31:46 +0900

    [feat] Add serializable data class

---
+++ Nextwin.Server/Protocol/SerializableData.cs
@@ -0,0 +1,24 @@
+﻿using MessagePack;
+
+namespace Nextwin.Server.Protocol
+{
+    /// <summary>
+    /// 데이터 전송을 위한 최상위 클래스로 SerializableData를 상속받는 모든 클래스의 필드변수의 Key는 1부터 시작
+    /// </summary>
+    [MessagePackObject]
+    public class SerializableData
+    {
+        [Key(0)]
+        public int MsgType { get; set; }
+
+        public SerializableData(int msgType)
+        {
+            MsgType = msgType;
+        }
+
+        public static int ReadMsgTypeFromBytes(byte[] bytes)
+        {
+            return bytes[1];
+        }
+    }
+}
